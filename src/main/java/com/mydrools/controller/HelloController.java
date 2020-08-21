43e56a0dc6bb1b37636ea696d8db428792ce1cbe
steveshenh <18510029708@163.com>
2020-08-21 14:47:50 +0800

    init a spring-boot based drools app

---
+++ src/main/java/com/mydrools/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.mydrools.controller;
+
+import com.mydrools.service.RuleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+    @Autowired
+    private RuleService ruleService;
+    @RequestMapping("/rule")
+    public String rule(){
+        ruleService.rule();
+        return "OK";
+    }
+}
+
